7b3ce546b5d6c19e6322bb7bb016abfa121adb93
Scolar02 <njugunascolar83@gmail.com>
2022-07-01 13:52:58 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
 int main (void)
 {
-	printf("with proper grammar, but the outcome is a work of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
