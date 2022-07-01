de7dd12a7e49b629c12287d03b7b99cf3276f29f
Scolar02 <njugunascolar83@gmail.com>
2022-07-01 13:27:42 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
  *
  *  * main - Entry point
@@ -11,9 +10,6 @@
  *     */
 int main(void)
 {
-
 			puts("\"Programming is like building a multilingual puzzle");
-
 						return (0);
-
 }
